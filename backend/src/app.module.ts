4cafc2dca058d3756f6fcb90cc8dbc9a5987bff6
maxim <maximka.lemish@gmail.com>
2024-04-09 01:24:41 +0300

    delete imports unnecessary Controller and Service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { WatermarkModule } from './watermark/watermark.module';
 
 @Module({
   imports: [WatermarkModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
